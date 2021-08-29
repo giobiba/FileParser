9b951ff9259ac0082dd81b9f11d988f9382cc630
Giobiba <g.blanar82@gmail.com>
2021-08-29 10:31:26 +0300

    corrected error

--- FileParserTests/ProgramTests.cs
+++ FileParserTests/ProgramTests.cs
@@ -2,6 +2,7 @@
 using FileParser;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace FileParserTests
 {
@@ -11,13 +12,14 @@ namespace FileParserTests
         [TestMethod]
         public void TestCsvFile()
         {
-            string startupPath = Environment.CurrentDirectory;
-            string csv_file = startupPath + "\\TestData\\data.csv";
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName; ;
+            string csv_file = projectDirectory + "\\TestData\\data.csv";
             string[] args = { csv_file };
             Program program = new Program(args);
             List<uint> expected = new List<uint>()
             {
-                3, 6, 25, 38, 50
+                3, 6, 8, 25, 38, 50
             };
 
             List<uint> actual = program.ParseFile(csv_file);
